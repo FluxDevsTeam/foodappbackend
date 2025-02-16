ed5a9a45ee810139d769ea6525d0ab6f2d390602
Masodi Success <109852676+suskidee@users.noreply.github.com>
2025-02-16 21:08:08 +0100

    edited vendor model file

--- vendor/models.py
+++ vendor/models.py
@@ -1,27 +1,10 @@
 from django.db import models
-from django.contrib.auth.models import User
-from django.utils.translation import gettext_lazy as _
+from django.conf import settings
 
 
-class Profile(models.Model):
-    USER_TYPES = [
-        ('customer', 'Customer'),
-        ('rider', 'Rider'),
-        ('vendor', 'Vendor'),
-    ]
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    phone = models.CharField(max_length=15, unique=True)
-    address = models.TextField(blank=True, null=True)
-    user_type = models.CharField(max_length=10, choices=USER_TYPES, default='customer')
-
-    def __str__(self):
-        return self.user.username
-
-
-# Vendor Model
 class Vendor(models.Model):
     name = models.CharField(max_length=255)
-    owner = models.OneToOneField(User, on_delete=models.CASCADE, related_name='vendor')
+    owner = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='vendor')
     address = models.TextField()
     phone = models.CharField(max_length=15, unique=True)
     is_active = models.BooleanField(default=True)
